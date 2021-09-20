784d4ab04f859aae58eb94787bbdf25b15fd8604
max anselmo <itmanselmo@gmail.com>
2021-09-20 12:21:54 -0400

    created variables according to the test suite that solve arithmetic questions, completing assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+
+let num3 = 4;
+let num4 = 5;
+
+let nums = [1, 20, 10, 5, 6];
+
+let multiply = num1 * num2;
+let random = Math.floor(Math.random() * 10) + 1;
+let mod = num3 % num4;
+let max = Math.max(...nums);
